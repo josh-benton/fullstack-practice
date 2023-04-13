f5f78b49af9874d75f7a9b29a609ca920e2b2e7f
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-04-13 16:32:16 -0400

    added get request handler for specific id in database

--- server.js
+++ server.js
@@ -10,20 +10,35 @@ const pool = new Pool({
 
 app.use(express.json());
 
+// Test get route
 app.get("/test", (req, res) => {
   res.json("Hello World!");
 });
 
+// Get route handler that queries against the database and responds with all rows
 app.get("/api/users", (req, res) => {
-    pool.query("SELECT * FROM users", (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("Error querying the database");
-      } else {
-        res.send(result.rows);
-      }
-    });
+  pool.query("SELECT * FROM users", (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error querying the database");
+    } else {
+      res.send(result.rows);
+    }
   });
+});
+
+// Get route handler that queries against the database and responds with a specific row
+app.get("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  pool.query("SELECT * FROM users WHERE id = $1", [userId], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal server error");
+    } else {
+      res.send(result.rows);
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Server is listening on port 3000...");

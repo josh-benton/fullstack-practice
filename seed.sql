900bd1ec263c8e91809ebeaf11d9f12fc85a4acb
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-04-13 16:06:37 -0400

    created migration.sql and seed.sql files

---
+++ seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, password) VALUES
+('John Smith', 'john@example.com', 'password1'),
+('Jane Doe', 'jane@example.com', 'password2'),
+('Bob Johnson', 'bob@example.com', 'password3');

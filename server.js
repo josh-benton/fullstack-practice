e11470fcce1bcf323dfb84f5a1d2a71df029a2ce
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-04-13 16:16:08 -0400

    added dependencies to project

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const dotenv = require('dotenv').config();
+const { Pool } = require('pg');
 
 app.get("/test", (req, res) => {
   res.json("Hello World!");

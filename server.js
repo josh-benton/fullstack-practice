f44e3b0d2b6dfe75af59aa7fc3cdfb540ca062de
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-04-13 16:18:43 -0400

    added get route handler to server to query against database

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const dotenv = require('dotenv').config();
-const { Pool } = require('pg');
+const dotenv = require("dotenv").config();
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+app.use(express.json());
 
 app.get("/test", (req, res) => {
   res.json("Hello World!");
 });
+
+app.get("/api/users", (req, res) => {
+    pool.query("SELECT * FROM users", (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error querying the database");
+      } else {
+        res.send(result.rows);
+      }
+    });
+  });
 
 app.listen(PORT, () => {
   console.log("Server is listening on port 3000...");
